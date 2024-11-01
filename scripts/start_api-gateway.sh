ed206e33865aba5171ffecbfd8d9dad65d3001a7
Luna <contact@aki-dev.com>
2024-11-01 04:22:47 +0100

    Finished Websocket endpoint and working on TTS and STT python API gateway

---
+++ scripts/start_api-gateway.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Navigate to the directory where api_gateway.py is located
+cd "../src/services/" || { echo "Directory not found"; exit 1; }
+
+# Activate virtual environment if needed
+source ../.venv/bin/activate  # Uncomment this line if you are using a virtual environment
+
+# Set PYTHONPATH to the project root
+export PYTHONPATH="$(dirname "$0")/.."
+
+# Set the FLASK_APP environment variable to the Flask app filename
+export FLASK_APP=api_gateway.py
+
+# Optionally set Flask to run in development mode (for debugging)
+# export FLASK_ENV=development
+
+# Start the Flask app
+flask run --host=0.0.0.0 --port=5000  # Change port if needed
